e1c4652e7aaa99d629fc3ff5072011e7612caa4c
lsuyogya@gmail.com <Suyogya>
2023-07-20 18:19:43 +0545

    removed nav

--- app/services/[id]/page.tsx
+++ app/services/[id]/page.tsx
@@ -5,53 +5,53 @@ export default function serviceID({ params }: { params: { id: number } }) {
   const data = {
     serviceName: "Product 1",
     serviceDetails: [
-      "Specially designed for Stockists, Wholesalers, Distributors, Pharma C&Fs",
+      "Specially designed for Stockists, Wholesalers",
       "Ensure 0% losses due to Expiry & Dump Stock",
       "Do effortless Stock Management & Accounting",
-      "Complete business management to increase your revenues",
-      "Uninterrupted GST Billing, Return Filing & E-way Bill",
-      "Manage multiple Warehouses & Godowns",
-      "Increase your sales with easy Shipping & Deliveries",
+      "Complete business management to increase revenues",
+      "Uninterrupted GST Billing, Return Filing & E-way",
+      "Manage multiple Warehouses & Godowns With Ease",
+      "Increase your sales with easy Shipping & Delivery",
     ],
   };
-  const formGroupClass = "md:flex md:items-center mb-6";
+  const formGroupClass = "grid md:items-center mb-6";
   const inputClass =
     "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline";
 
   return (
     <>
       <section className="p-4">
-        <div className="grid grid-cols-2 gap-20">
-          <div className="grid p-8 gap-20">
+        <div className="grid grid-cols-2 gap-1 ">
+          <div
+            className="grid p-8 gap-20"
+            style={{ borderRight: "1px solid #dfdfdf" }}>
             <div className=" font-bold text-xl place-self-center mt-6">
               {data.serviceName}
             </div>
             <ul className="grid gap-4 text-lg md:p-4">
               {data.serviceDetails.map((detail, index) => (
-                <li key={index}>{detail}</li>
+                <li className="list-disc" key={index}>
+                  {detail}
+                </li>
               ))}
             </ul>
           </div>
 
-          <div className="p-12">
-            <form className=" bg-gray-400 p-12 rounded grid">
+          <div className="p-12 " style={{ borderLeft: "1px solid #dfdfdf" }}>
+            <form className="p-24 rounded grid">
               <div className={formGroupClass}>
-                <div className="md:w-1/3">
-                  <label htmlFor="name">Name</label>
-                </div>
-                <div className="md:w-2/3">
-                  <input type="text" id="name" className={inputClass} />
-                </div>
+                <label htmlFor="name">Name</label>
+                <input type="text" id="name" className={inputClass} />
               </div>
               <div className={formGroupClass}>
-                <div className="md:w-1/3">
-                  <label htmlFor="phone">Phone Number</label>
-                </div>
-                <div className="md:w-2/3">
-                  <input type="text" id="phone" className={inputClass} />
-                </div>
+                <label htmlFor="phone">Phone Number</label>
+                <input type="text" id="phone" className={inputClass} />
               </div>
-              <button className="bg-green-500 p-4 rounded text-white w-32 place-self-center">
+              <div className={formGroupClass}>
+                <label htmlFor="pin">Pin</label>
+                <input type="text" id="pin" className={inputClass} />
+              </div>
+              <button className="bg-green-500 p-2 rounded text-white w-32 place-self-center">
                 Order Now
               </button>
             </form>
